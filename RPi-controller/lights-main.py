62ff6861dfb51a53679618d423d4ae0f11b5f209
peterthevicar <peterbcs@gmail.com>
2018-10-02 16:36:15 +0100

    initial upload

---
+++ RPi-controller/lights-main.py
@@ -0,0 +1,47 @@
+import time
+import sys
+sys.path.append('../ws2812-animator')
+from animator import anim_init,anim_define_pattern, anim_define_spot, anim_define_fade, anim_define_sparkle, anim_render, anim_set_brightness, RIGHT,LEFT,L2R1,STOP,REPEAT,REVERSE
+from gradients import gradient_preset, STEP, SMOOTH
+import numpy
+from colours import *
+import argparse
+
+def hue_256(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+    
+if __name__ == '__main__':
+	# Process arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+	args = parser.parse_args()
+
+	anim_init()
+	
+	print ('Press Ctrl-C to quit.')
+	if not args.clear:
+		print('Use "-c" argument to clear LEDs on exit')
+
+	try:
+		gra_desc = gradient_preset(5, STEP, 0, 1)
+		anim_define_pattern(gra_desc, 1, REVERSE, LEFT, 5, REVERSE)
+		anim_define_spot(1, RGB_Magenta, RIGHT, 1)
+		anim_define_fade(-1, SMOOTH, 150)
+		anim_set_brightness(255)
+		anim_define_sparkle(10, 0.1)
+		while True:
+			anim_render(time.time()+5) # let it run for a bit
+			gra_desc = gradient_preset(numpy.random.randint(1,9), STEP, 0, 1)
+			anim_define_pattern(gra_desc, 1, REVERSE, LEFT, 5, REVERSE)
+
+	except KeyboardInterrupt:
+		if args.clear:
+			print("Interrupted")
---
+++ web-server/form.php
@@ -0,0 +1,156 @@
+<?php
+//file_put_contents("form.json", json_encode(array('var1'=>'val1', 'var2'=>'val2')));
+$vars=json_decode(file_get_contents("json-current.json"), true);
+// This funtion outputs all the elements for a labelled drop-down (select)
+function select_input($select_label, $select_name, array $option) {
+    global $vars;
+    // Header with label
+    echo "<p>$select_label: <select name=\"$select_name\" id=\"$select_name\">\n";
+    // Put in all the options
+    foreach ($option as $option_value => $option_text) {
+        // $sel will be set to 'selected' for the selected option
+        $sel=($vars[$select_name]==$option_value)?' selected':'';
+        echo "  <option value=\"{$option_value}\"{$sel}>{$option_text}</option>\n";
+    }
+    // Finish off the select
+    echo "</select></p>\n";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style type="text/css">
+	th {color:blue; text-align:left}
+	td, th {width: 30%}
+	#curSel {color:white; background-color:blue; font-size:100%} /* Selected row in table */
+	.header {position: sticky; top: 0; background-color:white} /* will scroll to top of page then stop */
+	.footer {position: sticky; bottom: 0; width: 100%} /* stays at foot of page with text scrolling behind */
+	table {width:100%}
+	button {font-size:100%}
+	/* 
+	@media (min-width: 858px) {
+    html {
+        font-size: 1em;
+    }
+    */
+</style>
+<script>
+// https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable(tid,n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById(tid);
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+function selRow(thisrow) {
+	var cursel;
+	// un-select current one
+	cursel = document.getElementById("curSel")
+	if (cursel) {
+		cursel.id="";
+		cursel.firstChild.removeChild(cursel.firstChild.lastChild);
+	}
+	// Select new one
+	thisrow.id="curSel";
+	var newdiv = document.createElement("div");
+	newdiv.display = "block";
+	var newbut = document.createElement("button");
+	newbut.innerText = "DISPLAY";
+	newdiv.appendChild(newbut);
+	newbut = document.createElement("button");
+	newbut.innerText = "EDIT";
+	//newbut.style="margin-left:1em";
+	newdiv.appendChild(newbut);
+	thisrow.firstChild.appendChild(newdiv);
+	// For double click to select, this works but needs onclick to be 
+	// reset when id is reset above: 
+	//thisrow.onclick=function secondClick(){doDisplay();};
+}
+function doDisplay() {
+	var cursel;
+	cursel = document.getElementById("curSel")
+	if (cursel) {
+		alert("0="+cursel.children[0].innerText);
+	}
+}
+</script>
+</head>
+<body>
+	<div>
+		Here we go again!
+		<?php echo "<br>blob"; ?>
+		<?php echo "baah<br>";?>
+		<?php echo $vars["un"], $vars["tk"]; ?>
+	</div>
+	<div>
+		<table class="header">
+			<tr>
+				<!--When a header is clicked, run the sortTable function, with a parameter,
+				0 for sorting by names, 1 for sorting by country: -->
+				<th onclick="sortTable('ta',0)" style="width:30%">Col 1</th>
+				<th onclick="sortTable('ta',1)" style="width:30%">Col 2</th>
+			</tr>
+		</table>
+		<table id="ta">
+			<?php $r = 1; foreach ($vars[2] as $hist) {
+				echo "<tr  onclick='selRow(this)'><td style='width:30%'>",$hist[0],"</td><td style='width:30%'>",$hist[1],"</td></tr>";
+				$r++;
+			}
+			?>
+		</table>
+	</div>
+	<div class="footer">
+		<button type="button" onclick="doDisplay()">Display</button> 
+	</div>
+
+</body>
+</html>

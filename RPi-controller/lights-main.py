a5bd2c7829d253b37b175eddba51b55d666084bf
peterthevicar <peterbcs@gmail.com>
2018-10-30 20:59:06 +0000

    Better shut down

--- RPi-controller/lights-main.py
+++ RPi-controller/lights-main.py
@@ -7,6 +7,10 @@ import numpy
 from colours import *
 import urllib.request
 import json
+try:
+	import RPi.GPIO
+except:
+	print('Failed to import RPi.GPIO')
 
 # ["1"=>"Very slow", "2"=>"Slow", "3"=>"Medium", "4"=>"Fast", "5"=>"Very fast"]
 trans_speed = [1000.0, 40.0, 20.0, 5.0, 1.0, 0.5]
@@ -21,9 +25,18 @@ trans_spark = [0, 10, 20, 100, 250];
 # ["0"=>"None", "1"=>"Very slow", "2"=>"Slow", "3"=>"Medium", "4"=>"Fast"]
 trans_dmx_speed = [0, 10, 7, 4, 2];
 
+_gpio_chans = [17,27,22] # Three GPIO channels for: LEDs, DMX, Meteors
+_gpio_LED = 0
+_gpio_DMX = 1
+_gpio_MET = 2
+def init_gpio():
+	RPi.GPIO.setmode(RPi.GPIO.BCM)
+	RPi.GPIO.setup(_gpio_chans, RPi.GPIO.OUT, initial=False)
+
 if __name__ == '__main__':
 	cur_id = "" # Current display ID (to spot changes)
 	try:
+		init_gpio()
 		while True:
 			try:
 				download = urllib.request.urlopen('http://192.168.1.10/web-server/de-q.php')
@@ -31,18 +44,21 @@ if __name__ == '__main__':
 				text = data.decode('utf-8') # convert to a 'str' object
 			except:
 				print("DEBUG:main:31 error reading de-q")
-				text = '{"co": ["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#ff00ff"], "br": "255", "hd": ["Rainbow", "Peter", "90bfbf8c", 1539615910, 0, 0], "id": "id1", "st": ["0", "#062af9", "1", "3", "2"], "durn": 5, "se": ["4", "2", "2", "2", "2"], "gr": ["1", "1", "0"], "fq": "0", "fa": ["0", "1", "3"], "me": ["1"], "fl": ["1", "#000000", "#ffffff", "1", "3"], "sk": ["1", 8.3]}'
+				text = '{"co": ["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#ff00ff"], "br": "80", "hd": ["Rainbow", "Peter", "90bfbf8c", 1539615910, 0, 0], "id": "id1", "st": ["0", "#062af9", "1", "3", "2"], "durn": 5, "se": ["4", "2", "2", "2", "2"], "gr": ["1", "1", "0"], "fq": "0", "fa": ["0", "1", "3"], "me": ["1"], "fl": ["1", "#000000", "#ffffff", "1", "3"], "sk": ["1", 8.3]}'
 
 			print("DEBUG:main:33 text=",text)
 			spec = json.loads(text)
 			print("DEBUG:main:36 id=",spec['id'])
 			if spec['id'] == 'sid0': # switch everything off and wait
+				RPi.GPIO.output(_gpio_chans, False) # Power down the mains supplies
 				anim_stop()
 				# ~ TODO: hardware control of power
 				time.sleep(spec['durn'])
 			else:
+				RPi.GPIO.output(_gpio_chans[_gpio_LED], True) # Make sure the mains is on
+				led_max_brightness = int(spec['br'])
 				if spec['id'] != cur_id or spec['fq'] == '1': # Need to read the parameters for the new display
-					anim_init(led_count=150*4, max_brightness=180)
+					anim_init(led_count=150*4, max_brightness=led_max_brightness)
 					print ('DEBUG:main:41 spec=', spec)
 					print ('DEBUG:main:4 spec["co"]=', spec['co'])
 					
@@ -68,11 +84,12 @@ if __name__ == '__main__':
 					
 					# Sparkle
 					anim_define_sparkle(s_per_k=trans_spark[int(spec['sk'][0])], s_duration=0.1)
-					dmx_mode = int(spec['fl'][0])
 					
 					# DMX lights
+					dmx_mode = int(spec['fl'][0])
+					RPi.GPIO.output(_gpio_chans[_gpio_LED], dmx_mode != 0) # Switch on DMX mains unless set to off
 					dmx_secs = trans_dmx_speed[int(spec['fl'][4])]
-					print('DEBUG:main:75 dmx_secs=',dmx_secs)
+					#~ print('DEBUG:main:92 dmx_secs=',dmx_secs)
 					if int(spec['fl'][3]) == 1: dmx_secs *= 2 # Twice as long for fade
 					if dmx_mode == 0: # off
 						dmx_posv=[]
@@ -84,16 +101,19 @@ if __name__ == '__main__':
 						dmx_posv = [0, 50]
 						dmx_mode = 2
 					anim_define_dmx(d_off_auto_indep=dmx_mode, d_posv=dmx_posv, d_secs=dmx_secs, d_gradient_desc=GradientDesc([int(spec['fl'][1][1:],16),int(spec['fl'][2][1:],16)], 1, int(spec['fl'][3]), bar_on=0))
+					
+					# Meteors - just switch on or off at the mains
+					RPi.GPIO.output(_gpio_chans[_gpio_MET], spec['fl'][0] == '1')
 
 					cur_id = spec['id']
+					
 				anim_set_max_brightness(int(spec['br'])) # can change via sysctl interface
 				anim_render(time.time()+int(spec['durn'])) # run until we need to check back
 
-	except KeyboardInterrupt:
-			print("Interrupted")
-	anim_stop()
-
-# TODO
-# CHECK the parameters and do something sensible on error
-# What happens on a crash? (maybe external)
-# What happens if you can't read from the internet?
+	except:
+		print('ERROR:main:99 Exception')
+		raise
+	finally:
+		anim_stop()
+		RPi.GPIO.cleanup()
+		raise

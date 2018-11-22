c2c39f7c938a6dd0154eb79dd523197a92466172
peterthevicar <peterbcs@gmail.com>
2018-11-22 13:32:28 +0000

    Heading for error-check

--- web-server/s-error-check.php
+++ web-server/s-error-check.php
@@ -5,7 +5,22 @@ include "s-error-handler.php";
 touch('ts-error-check');
 ?>
 <html>
+	<style>
+	.header { /* will scroll to top of page then stop */	
+		position: sticky; top: 0;
+		background-color:rgb(192, 159, 128);
+		color:white;
+		padding: 15px 15px 15px 5px;
+		border: none;
+		text-align: left;
+		outline: none;
+
+	}
+	</style>
 	<body>
+		<div class="header">
+			error-log.txt
+		</div>
 		<div id="errorlog">
 			<?php echo nl2br(file_get_contents('error-log.txt')); ?>
 		</div>

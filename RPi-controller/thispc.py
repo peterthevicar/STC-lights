f7486ce6c3bc4ab0664a61197c65b9e4b80cbbf3
peterthevicar <peterbcs@gmail.com>
2020-12-01 16:30:00 +0000

    ignore thispc.py file in the settings subdir - this is created for each different installation thispc-py - default values for various globals

---
+++ RPi-controller/thispc.py
@@ -0,0 +1,8 @@
+"""
+This file has all the variables which change between different setups
+(e.g. number of LEDS, DMX channels etc)
+This one in the same directory as lights-main gives the default values,
+to override put a copy into the settings subdirectory
+"""
+NUM_LEDS=540+2 # 2 to act as drivers and signal conditioners in the box
+HAVE_DMX_HARDWARE = True

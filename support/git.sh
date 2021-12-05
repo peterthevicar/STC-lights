dbb4aaabf37a37e25d4171a4e6aba60482df84ad
peterthevicar <peterbcs@gmail.com>
2021-12-05 18:07:36 +0000

    Summary of useful git commands in git.sh

---
+++ support/git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "
+Useful git commands:
+  Current status: git status
+  Revert to current state of remote: git checkout .; git pull
+  Find differences from what's committed: git diff
+  Add all new files: git add --all
+  Commit all changes: git commit -a
+  Push changes: git push; git fetch # Bring local git up to date with remote
+"	
